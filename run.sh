6cd1b75c11a3ce3bea843cb6a83fb64a5698d090
Tom McGrath <thomas.m.mcgrath@gmail.com>
2019-02-05 12:59:33 +0000

    Changed run script

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
-echo "Loading Global Priorities"
-python priorities.py > priorities.csv
 echo "Accessing News Service"
-python get_articles.py priorities.csv > articles_with_keywords.csv
-echo "Computing QALY scores"
+python get_articles.py global_prios/global_prios.csv > articles_with_keywords.csv
+echo "Computing QALY scores from global priorities"
 python score_articles.py priorities.csv articles_with_keywords.csv > ranked_articles.csv
