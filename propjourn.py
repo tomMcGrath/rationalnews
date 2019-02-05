4a4068c2b96c365738d6986b8e6478268a36d5ea
sloomba <94.sahilloomba@gmail.com>
2019-02-05 14:42:01 +0000

    func to parse keyword logic

---
+++ propjourn.py
@@ -0,0 +1,32 @@
+def parse_keywords(x):
+    """Parses given string to generate a list of keywords. For example:
+    
+    >>> parse_keywords('brain OR breast AND tumour OR tumor OR cancer | heart OR lung OR brain AND disease OR disorder OR issue')
+    [['brain tumour',
+      'brain tumor',
+      'brain cancer',
+      'breast tumour',
+      'breast tumor',
+      'breast cancer'],
+     ['heart disease',
+      'heart disorder',
+      'heart issue',
+      'lung disease',
+      'lung disorder',
+      'lung issue',
+      'brain disease',
+      'brain disorder',
+      'brain issue']]
+    """
+    from itertools import product
+    tokens = x.split('|')
+    toks_all = list()
+    for token in tokens:
+        toks = token.split('AND')
+        toks_curr = list()
+        for tok in toks:
+            t = tok.split('OR')
+            t = [s.strip() for s in t]
+            toks_curr.append(t)
+        toks_all.append([' '.join(i).strip().lower() for i in product(*toks_curr, repeat=1)])        
+    return toks_all
