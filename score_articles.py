bdde34d9f8c30f9dfd206cba7e1608acdedf98bb
jaryaman <j.aryaman25@gmail.com>
2019-02-18 21:03:45 +0000

    Sample on log-space and tweet topics

--- score_articles.py
+++ score_articles.py
@@ -10,7 +10,6 @@ def get_qaly_data(filepath):
             topic, score, keywords, ref = line.split(',')
             #keywords = keywords.split(' AND ')
             #keywords = tuple([i.strip() for i in keywords]) # remove whitespace
-            #set_trace()
             keywords_set = parse_keywords(keywords)
             for keywords in keywords_set:
             	qaly_scorer[tuple(keywords)] = (float(score), topic)
@@ -19,18 +18,18 @@ def get_qaly_data(filepath):
 
 def score_article(article, qaly_scorer):
     article_score = 0
-
+    article_topics = []
     for keyword_set in qaly_scorer:
         if all(keyword in article for keyword in keyword_set):
             article_score += qaly_scorer[keyword_set][0]
-
-    return article_score
+            article_topics.append(qaly_scorer[keyword_set][1])
+    return article_score, article_topics
 
 def score_all(article_dict, qaly_scorer):
     for article_url in article_dict:
         article_content = article_dict[article_url]
-        article_score = score_article(article_content, qaly_scorer)
+        article_score, article_topics = score_article(article_content, qaly_scorer)
 
-        article_dict[article_url] = [article_score, article_content]
+        article_dict[article_url] = [article_score, article_content,article_topics]
 
     return article_dict
--- tweeting.py
+++ tweeting.py
@@ -29,7 +29,7 @@ class RepeatEvery(threading.Thread):
     def stop(self):
         self.runable = False
 
-def tweet_news(tweepyapi,qaly_path,error_log_filename, error_log_pointer, load_articles = False, qaly_thresh = 1.0):
+def tweet_news(tweepyapi,qaly_path,error_log_filename, error_log_pointer, load_articles = False, qaly_thresh = 1.0, sample_log_qalys=True, dbg_mode=False):
     """
     Tweet a single news story drawn randomly, weighted by a QALY
 
@@ -41,49 +41,56 @@ def tweet_news(tweepyapi,qaly_path,error_log_filename, error_log_pointer, load_a
     error_log_pointer : An IO pointer, the pointer to the error log
     load_articles : A bool, if true, load a database of URLs
     qaly_thresh : A float, threshold on qalys to tweet
+    sample_log_qalys : A bool, sample the qalys in log-space
+    dbg_mode : A bool, enter debug mode. Samples fewer pages from the API, since we have a daily budget
     """
     if load_articles:
         pickled_file = open('dict_url_desc_out.pkl', 'rb')
         article_dict = pickle.load(pickled_file)
     else:
-        # page_limit_per_request = 1
-        # results_per_page = 20
-        # article_dict = get_articles.get_results()
-        article_dict = get_articles.get_results()
+        if dbg_mode:
+            article_dict = get_articles.get_results(page_limit_per_request = 1)
+        else:
+            article_dict = get_articles.get_results()
     if len(article_dict) < 5: # assume something went wrong and load
         error_log_pointer = open(error_log_filename,'a')
-        error_log_pointer.write('get_articles() error,'+str(datetime.datetime.now())+'\n')
+        error_log_pointer.write('get_articles() error,'+str(datetime.datetime.now())+',NaN'+'\n')
         error_log_pointer.close()
         print('Error in get_articles()\n')
     else:
         ## Calculate aggregate QALY scores for each article
-        # BUG: score_articles.get_qaly_data fails for the trivial case of X AND Y AND Z
         qaly_scorer = score_articles.get_qaly_data(qaly_path)
         article_dict = score_articles.score_all(article_dict, qaly_scorer)
-
         v = article_dict.values()
         v = list(v)
-        qalys_scores = np.array([ a for a,b in v ] )
+        qalys_scores = np.array([ a[0] for a in v ] )
         qaly_total = qalys_scores.sum()
         if qaly_total < qaly_thresh: # there aren't enough newsworthy stories
             output=tweepyapi.update_status("I didn't find anything interesting at " + str(datetime.datetime.now()))
             error_log_pointer = open(error_log_filename,'a')
-            error_log_pointer.write("No news"+','+str(datetime.datetime.now())+'\n')
+            error_log_pointer.write("No news"+','+str(datetime.datetime.now())+',NaN'+'\n')
             error_log_pointer.close()
             print('No news\n')
             return
+        if sample_log_qalys:
+            qalys_scores = np.log(qalys_scores + 1.0) # sample qalys in log-space
+
         article_choice_index = np.random.choice(len(qalys_scores), p=qalys_scores/qalys_scores.sum())
         url = list(article_dict.keys())[article_choice_index]
-
-        #set_trace()
+        topics = v[article_choice_index][2]
+        topics_string = ''
+        for i, topic in enumerate(topics):
+            if i == len(topics) - 1:
+                topics_string+=topic
+            else:
+                topics_string+=topic+'; '
         try:
-            output=tweepyapi.update_status(url)
+            output=tweepyapi.update_status(topics_string + '\n' + url)
             error_log_pointer = open(error_log_filename,'a')
-            error_log_pointer.write('Success,'+str(datetime.datetime.now())+'\n')
+            error_log_pointer.write('Success,'+str(datetime.datetime.now())+','+topics_string+'\n')
             error_log_pointer.close()
         except Exception as e:
-            # To do: this is bad practice, shouldn't catch all possible exceptions
             error_log_pointer = open(error_log_filename,'a')
-            error_log_pointer.write(e.reason+','+str(datetime.datetime.now())+'\n')
+            error_log_pointer.write(e.reason+','+str(datetime.datetime.now())+',NaN'+'\n')
             error_log_pointer.close()
     print('Done!')
